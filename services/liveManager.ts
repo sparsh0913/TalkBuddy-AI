12d8b248ebdb9ef2b6fd0d53b24584a92469a959
Sparsh Agarwal <sparshagarwal190@gmail.com>
2026-05-30 22:46:58 +0530

    creating session with Gemini live API

--- services/liveManager.ts
+++ services/liveManager.ts
@@ -1,6 +1,37 @@
-export class LiveManager {
+import { MODEL } from '@/lib/constants';
+import { GoogleGenAI, Modality, Session } from '@google/genai';
 
-    startSession(){
-        console.log("starting the session")
+export class LiveManager {
+private ai: GoogleGenAI;
+private activeSession: Session | null = null;
+
+constructor(){
+this.ai = new GoogleGenAI({
+  apiKey:process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+});
+}
+   async startSession(){
+    const config = { 
+      responseModalities: [Modality.AUDIO],
+      systemInstruction : "You are a helpful and friendly AI Assisant" };
+this.activeSession = await this.ai.live.connect({
+    model: MODEL,
+    callbacks: {
+      onopen: function () {
+        console.debug('Opened');
+      },
+      onmessage: function (message) {
+        console.debug(message);
+      },
+      onerror: function (e) {
+        console.debug('Error:', e.message);
+      },
+      onclose: function (e) {
+        console.debug('Close:', e.reason);
+      },
+    },
+    config: config,
+  });
+  console.log("session", this.activeSession);
     }
 }
